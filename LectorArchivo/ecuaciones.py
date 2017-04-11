2ffa8c48b441149e2c54cef532baf422b812d971
Mauricio Neira <m.neira10@uniandes.edu.co>
2017-04-11 16:02:59 -0500

    pusheando para el viejo juanca

--- LectorArchivo/ecuaciones.py
+++ LectorArchivo/ecuaciones.py
@@ -6,16 +6,21 @@ b=3
 
 c=5
 
-const = {'c': 5.0, 'a': 0.0, 'b': 3.0}
+const = {'a': 0.0, 'b': 3.0, 'c': 5.0}
 #valores iniciales
-initVals = [3.0, 2.0]
+initVals = [3.0, 4.0]
 #parametros en el diccionario
-params = {'x': 'xs[0]', 'y': 'xs[1]'}
+params = {'y': 'xs[1]', 'x': 'xs[0]'}
 #parametros en el diccionario reversados
-revparams = {'xs[0]': 'x', 'xs[1]': 'y'}
+revparams = {'xs[1]': 'y', 'xs[0]': 'x'}
 #ecuaciones diferenciales
 def x(xs,const,t):
 	return -3*xs[0]*4*xs[1]  -2  +a 
 def y(xs,const,t):
 	return -3*xs[1]*4*xs[0]  -2 
-funcArray = [x, y]
+funcArray = [y, x]
+def darEventos(xs):
+	return [-3*xs[0]*4*xs[1] ,-2 ,+a,-3*xs[1]*4*xs[0] ,-2]
+funcArray = [y, x]
+def darEventos(xs):
+	return [-3*xs[0]*4*xs[1] ,-2 ,+a,-3*xs[1]*4*xs[0] ,-2]
